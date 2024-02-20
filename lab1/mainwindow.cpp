0e524372573e59b1cb18189f938da08e84c0cf26
satoshi <uglysatoshi@ya.ru>
2024-02-20 21:29:42 +0300

    lab1 rebuild

--- lab1/mainwindow.cpp
+++ lab1/mainwindow.cpp
@@ -16,100 +16,227 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_plus_clicked() {
-    double x1 = ui-> line_1 -> text().toDouble();
-    double x2 = ui-> line_2 -> text().toDouble();
+void MainWindow::on_b1_clicked()
+{
     QString s;
-    s.sprintf("%.4f", x1 + x2);
-    ui -> result -> setText(s);
+    s = ui -> expression -> text();
+    ui -> expression -> setText(s+"1");
 }
 
-void MainWindow::on_multy_clicked() {
-    double x1 = ui-> line_1 -> text().toDouble();
-    double x2 = ui-> line_2 -> text().toDouble();
+void MainWindow::on_b2_clicked()
+{
     QString s;
-    s.sprintf("%.4f", x1 * x2);
-    ui -> result -> setText(s);
+    s = ui -> expression -> text();
+    ui -> expression -> setText(s+"2");
+}
+
+void MainWindow::on_b3_clicked()
+{
+    QString s;
+    s = ui -> expression -> text();
+    ui -> expression -> setText(s+"3");
+}
+
+
+void MainWindow::on_b4_clicked()
+{
+    QString s;
+    s = ui -> expression -> text();
+    ui -> expression -> setText(s+"4");
+}
+
+void MainWindow::on_b5_clicked()
+{
+    QString s;
+    s = ui -> expression -> text();
+    ui -> expression -> setText(s+"5");
+}
+
+void MainWindow::on_b6_clicked()
+{
+    QString s;
+    s = ui -> expression -> text();
+    ui -> expression -> setText(s+"6");
+}
+
+void MainWindow::on_b7_clicked()
+{
+    QString s;
+    s = ui -> expression -> text();
+    ui -> expression -> setText(s+"7");
+}
+
+void MainWindow::on_b8_clicked()
+{
+    QString s;
+    s = ui -> expression -> text();
+    ui -> expression -> setText(s+"8");
+}
+
+void MainWindow::on_b9_clicked()
+{
+    QString s;
+    s = ui -> expression -> text();
+    ui -> expression -> setText(s+"9");
+}
+
+void MainWindow::on_b0_clicked()
+{
+    QString s;
+    s = ui -> expression -> text();
+    ui -> expression -> setText(s+"0");
 }
 
 void MainWindow::on_divide_clicked()
 {
-    double x1 = ui-> line_1 -> text().toDouble();
-    double x2 = ui-> line_2 -> text().toDouble();
     QString s;
-    s.sprintf("%.4f", x1 / x2);
-    ui -> result -> setText(s);
+    s = ui -> expression -> text();
+    if ((s.contains("+") || s.contains("-") || s.contains("*") || s.contains("/")) || !(s.contains("1") || s.contains("2") || s.contains("3") || s.contains("4") || s.contains("5") || s.contains("6") || s.contains("7") || s.contains("8") || s.contains("9") || s.contains("0"))) {
+        ui -> expression -> setText(s);
+    } else {
+        ui -> expression -> setText(s+"/");
+    }
+}
+
+void MainWindow::on_multiply_clicked()
+{
+    QString s;
+    s = ui -> expression -> text();
+    if ((s.contains("+") || s.contains("-") || s.contains("*") || s.contains("/")) || !(s.contains("1") || s.contains("2") || s.contains("3") || s.contains("4") || s.contains("5") || s.contains("6") || s.contains("7") || s.contains("8") || s.contains("9") || s.contains("0"))) {
+        ui -> expression -> setText(s);
+    } else {
+        ui -> expression -> setText(s+"*");
+    }
 }
 
 void MainWindow::on_minus_clicked()
 {
-    double x1 = ui-> line_1 -> text().toDouble();
-    double x2 = ui-> line_2 -> text().toDouble();
     QString s;
-    s.sprintf("%.4f", x1-x2);
-    ui -> result -> setText(s);
+    s = ui -> expression -> text();
+    if ((s.contains("+") || s.contains("-") || s.contains("*") || s.contains("/")) || !(s.contains("1") || s.contains("2") || s.contains("3") || s.contains("4") || s.contains("5") || s.contains("6") || s.contains("7") || s.contains("8") || s.contains("9") || s.contains("0"))) {
+        ui -> expression -> setText(s);
+    } else {
+        ui -> expression -> setText(s+"-");
+    }
 }
 
-void MainWindow::on_sin_1_clicked()
+void MainWindow::on_plus_clicked()
 {
-    double x1 = ui-> line_1 -> text().toDouble();
     QString s;
-    s.sprintf("%.4f", sin(x1));
-    ui -> line_1 -> setText(s);
-}
-
-void MainWindow::on_sin_2_clicked() {
-    double x2 = ui-> line_2 -> text().toDouble();
-    QString s;
-    s.sprintf("%.4f", sin(x2));
-    ui -> line_2 -> setText(s);
-}
-
-void MainWindow::on_pow_1_clicked() {
-    double x1 = ui-> line_1 -> text().toDouble();
-    QString s;
-    s.sprintf("%.4f", x1 * x1);
-    ui -> line_1 -> setText(s);
-}
-
-void MainWindow::on_pow_2_clicked() {
-    double x2 = ui-> line_2 -> text().toDouble();
-    QString s;
-    s.sprintf("%.4f", x2 * x2);
-    ui -> line_2 -> setText(s);
+    s = ui -> expression -> text();
+    if ((s.contains("+") || s.contains("-") || s.contains("*") || s.contains("/")) || !(s.contains("1") || s.contains("2") || s.contains("3") || s.contains("4") || s.contains("5") || s.contains("6") || s.contains("7") || s.contains("8") || s.contains("9") || s.contains("0")) ) {
+        ui -> expression -> setText(s);
+    } else {
+        ui -> expression -> setText(s+"+");
+    }
 }
 
 
-
-void MainWindow::on_sqrt_1_clicked()
+void MainWindow::on_clear_clicked()
 {
-    double x1 = ui-> line_1 -> text().toDouble();
+    ui -> expression -> setText("");
+    ui -> result -> setText("");
+}
+
+
+
+void MainWindow::on_del_clicked()
+{
     QString s;
-    s.sprintf("%.4f", sqrt(x1));
-    ui -> line_1 -> setText(s);
+    s = ui -> expression -> text();
+    s.remove(s.length()-1, s.length());
+    ui -> expression -> setText(s);
 }
 
-void MainWindow::on_sqrt_2_clicked()
+
+
+void MainWindow::on_x2_clicked()
 {
-    double x2 = ui-> line_2 -> text().toDouble();
-     QString s;
-     s.sprintf("%.4f", sqrt(x2));
-     ui -> line_2 -> setText(s);
+    QString s;
+    s = ui -> expression -> text();
+    if ((s.contains("+") || s.contains("-") || s.contains("*") || s.contains("/")) || !(s.contains("1") || s.contains("2") || s.contains("3") || s.contains("4") || s.contains("5") || s.contains("6") || s.contains("7") || s.contains("8") || s.contains("9") || s.contains("0")) ) {
+        ui -> expression -> setText(s);
+    } else {
+        double x = s.toDouble() * s.toDouble();
+        s.setNum(x);
+        ui -> result -> setText(s);
+    }
 }
 
-void MainWindow::on_log_1_clicked()
+
+void MainWindow::on_log_clicked()
 {
-    double x1 = ui-> line_1 -> text().toDouble();
-     QString s;
-     s.sprintf("%.4f", log(x1));
-     ui -> line_1 -> setText(s);
+    QString s;
+    s = ui -> expression -> text();
+    if ((s.contains("+") || s.contains("-") || s.contains("*") || s.contains("/")) || !(s.contains("1") || s.contains("2") || s.contains("3") || s.contains("4") || s.contains("5") || s.contains("6") || s.contains("7") || s.contains("8") || s.contains("9") || s.contains("0")) ) {
+        ui -> expression -> setText(s);
+    } else {
+        double x = log(s.toDouble());
+        s.setNum(x);
+        ui -> result -> setText(s);
+    }
 }
 
-void MainWindow::on_log_2_clicked()
+void MainWindow::on_sin_clicked()
 {
-    double x2 = ui-> line_2 -> text().toDouble();
-     QString s;
-     s.sprintf("%.4f", log(x2));
-     ui -> line_2 -> setText(s);
+    QString s;
+    s = ui -> expression -> text();
+    if ((s.contains("+") || s.contains("-") || s.contains("*") || s.contains("/")) || !(s.contains("1") || s.contains("2") || s.contains("3") || s.contains("4") || s.contains("5") || s.contains("6") || s.contains("7") || s.contains("8") || s.contains("9") || s.contains("0")) ) {
+        ui -> expression -> setText(s);
+    } else {
+        double x = sin(s.toDouble());
+        s.setNum(x);
+        ui -> result -> setText(s);
+    }
 }
 
+void MainWindow::on_dB_clicked()
+{
+    QString s;
+    s = ui -> expression -> text();
+    if ((s.contains("+") || s.contains("-") || s.contains("*") || s.contains("/")) || !(s.contains("1") || s.contains("2") || s.contains("3") || s.contains("4") || s.contains("5") || s.contains("6") || s.contains("7") || s.contains("8") || s.contains("9") || s.contains("0")) ) {
+        ui -> expression -> setText(s);
+    } else {
+        double x = 10 * log(s.toDouble()/0.001);
+        s.setNum(x);
+        ui -> result -> setText(s);
+    }
+}
+
+void MainWindow::on_equals_clicked()
+{
+    QString s;
+    s = ui -> expression -> text();
+    if (s.contains("+")) {
+        double x1, x2;
+        QStringList l = s.split("+");
+        x1 = l.first().toDouble();
+        x2 = l.last().toDouble();
+        ui->result->setText(s.setNum(x1+x2));
+    }
+    if (s.contains("-")) {
+        double x1, x2;
+        QStringList l = s.split("-");
+        x1 = l.first().toDouble();
+        x2 = l.last().toDouble();
+        ui->result->setText(s.setNum(x1-x2));
+    }
+    if (s.contains("/")) {
+        double x1, x2;
+        QStringList l = s.split("/");
+        x1 = l.first().toDouble();
+        x2 = l.last().toDouble();
+        if (x2 == 0) {
+            ui->result->setText("Divided by zero");
+        } else {
+            ui->result->setText(s.setNum(x1/x2));
+        }
+    }
+    if (s.contains("*")) {
+        double x1, x2;
+        QStringList l = s.split("*");
+        x1 = l.first().toDouble();
+        x2 = l.last().toDouble();
+        ui->result->setText(s.setNum(x1*x2));
+    }
+}
--- lab1/mainwindow.h
+++ lab1/mainwindow.h
@@ -16,29 +16,47 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_plus_clicked();
+    void on_b1_clicked();
 
-    void on_multy_clicked();
+    void on_b2_clicked();
+
+    void on_b3_clicked();
+
+    void on_b4_clicked();
+
+    void on_b5_clicked();
+
+    void on_b6_clicked();
+
+    void on_b7_clicked();
+
+    void on_b8_clicked();
+
+    void on_b9_clicked();
+
+    void on_b0_clicked();
 
     void on_divide_clicked();
 
-    void on_sin_1_clicked();
-
-    void on_sin_2_clicked();
-
-    void on_pow_1_clicked();
-
-    void on_pow_2_clicked();
+    void on_multiply_clicked();
 
     void on_minus_clicked();
 
-    void on_sqrt_1_clicked();
+    void on_plus_clicked();
 
-    void on_sqrt_2_clicked();
+    void on_clear_clicked();
 
-    void on_log_1_clicked();
+    void on_del_clicked();
 
-    void on_log_2_clicked();
+    void on_x2_clicked();
+
+    void on_log_clicked();
+
+    void on_sin_clicked();
+
+    void on_dB_clicked();
+
+    void on_equals_clicked();
 
 private:
     Ui::MainWindow *ui;
