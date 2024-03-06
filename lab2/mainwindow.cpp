c84027e8741b1357f78b0c8f054cfb93077f3acc
satoshi <uglysatoshi@ya.ru>
2024-03-06 12:21:36 +0300

    lab2

--- lab2/mainwindow.cpp
+++ lab2/mainwindow.cpp
@@ -2,32 +2,77 @@
 #include "ui_mainwindow.h"
 #include <QMessageBox>
 
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
+    ui -> setupUi(this);
     m_db = QSqlDatabase::addDatabase("QSQLITE");
-    m_db.setDatabaseName("myDB");
+    m_db.setDatabaseName("Petrovich");
     query = new QSqlQuery(m_db);
+    m_db.open();
     if(!m_db.open())
         throw "can't open database";
-    if(!m_db.tables().contains("Person")) {
+    if(!m_db.tables().contains("Person")){
         query->clear();
-        query->exec("CREATE TABLE Person(ID INTEGER PRIMARY KEY, Name VARCHAR, Year INTEGER);");
+        query->exec("CREATE Table Petrovich(ID INTEGER PRIMARY KEY, Item VARCHAR, Type VARCHAR, Amount INTEGER);");
         query->clear();
-        query->exec("INSERT INTO Person (ID,Name,Year) VALUES (1,'Ann', 2000);");
+        query->exec("INSERT INTO Petrovich(ID,Item,Type,Amount) VALUES (1,'Помидоры', 'Овощи', 53);");
         query->clear();
-        query->exec("INSERT INTO Person (ID,Name,Year) VALUES (2,'Jim', 2003);");
+        query->exec("INSERT INTO Petrovich(ID,Item,Type,Amount) VALUES (2,'Баклажаны', 'Овощи', 52);");
+        query->clear();
+        query->exec("INSERT INTO Petrovich(ID,Item,Type,Amount) VALUES (3,'Арбузы', 'Ягоды', 30);");
     }
     model = new QSqlTableModel(this,m_db);
-    model->setTable("Person");
+    model->setTable("Petrovich");
     model->select();
     model->setEditStrategy(QSqlTableModel::OnFieldChange);
     ui->tableView->setModel(model);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
     delete query;
     delete model;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    model->setFilter("Amount<50");
+    model->select();
+    ui->tableView->setModel(model);
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    model->setFilter("");
+    model->select();
+    ui->tableView->setModel(model);
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    model->setFilter("Amount>50");
+    model->select();
+    ui->tableView->setModel(model);
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    if (ui->Item->text().isEmpty() || ui->Amout->text().isEmpty() || ui->Type->text().isEmpty()|| ui->ID->text().isEmpty()){
+        return;
+    } else {
+        query->clear();
+        QString id = ui->ID->text();
+        QString item = ui->Item->text();
+        QString amout = ui->Amout->text();
+        QString type = ui->Type->text();
+        QString buf = tr("INSERT INTO Petrovich (ID,Item,Type,Amount) VALUES (")+id+tr(",'")+item+tr("','")+type+tr("',")+amout+tr(");");
+        query->exec(buf);
+        model->select();
+    }
+}
+
+void MainWindow::on_horizontalSlider_valueChanged(int value)
+{
+    model->setFilter(QString("Amount>%1").arg(value));
+    model->select();
+    ui->tableView->setModel(model);
 }
--- lab2/mainwindow.h
+++ lab2/mainwindow.h
@@ -16,6 +16,17 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_horizontalSlider_valueChanged(int value);
+
 private:
     Ui::MainWindow *ui;
     QSqlDatabase m_db;
